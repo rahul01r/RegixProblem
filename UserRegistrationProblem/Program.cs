099eb8882d255ae3ae7c52a08047fc682bbaf918
rahul01r <rrdubeynibm@gmail.com>
2022-11-20 12:02:14 +0530

    [rahul] Add:UC1-ValidationFirstName

---
+++ UserRegistrationProblem/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace UserRegistrationProblem
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to User Registration Problems Statement Using REGEX");
+            UserValidation userValidation = new UserValidation();
+            Console.WriteLine("Enter The First Name: ");
+            string firstname = Console.ReadLine();
+            userValidation.ValidationFirstName(firstname);
+        }
+    }
+}
---
+++ UserRegistrationProblem/UserValidation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistrationProblem
+{
+    public class UserValidation
+    {
+        public const string FIRST_NAME_REGEX = "^[A-Z][a-zA-Z]{2}$";
+        public void ValidationFirstName(string firstname)
+        {
+            Regex regex = new Regex(FIRST_NAME_REGEX);
+            bool result = regex.IsMatch(firstname);
+            Console.WriteLine(result);
+        }
+    }
+}
