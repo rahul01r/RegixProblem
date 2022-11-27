1134ba236c588c400af2136e0037ade68a4d3dec
rahul01r <rrdubeynibm@gmail.com>
2022-11-27 18:41:29 +0530

    [rahul] Add:UC13-LambdaExpression

--- UserRegistrationProblem/Program.cs
+++ UserRegistrationProblem/Program.cs
@@ -5,34 +5,26 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to User Registration Problems Statement Using REGEX");
-            UserValidation userValidation = new UserValidation();
-            Console.WriteLine("Enter The First Name: ");
-            string firstname = Console.ReadLine();
-            userValidation.ValidationFirstName(firstname);
-
-            Console.WriteLine("Enter The Last Name: ");
+            //User FirstName
+            Console.WriteLine("Enter the firstname: ");
+            string FirstName = Console.ReadLine();
+            Console.WriteLine(UserValidation.CheckFirstName(FirstName));
+            //User LastName
+            Console.WriteLine("Enter the lastname: ");
             string lastname = Console.ReadLine();
-            userValidation.ValidationLastName(lastname);
-
-            Console.WriteLine("Enter Email: ");
+            Console.WriteLine(UserValidation.CheckLastName(lastname));
+            //User Email
+            Console.WriteLine("Enter the email: ");
             string email = Console.ReadLine();
-            userValidation.ValidationFirstName(email);
-
-            Console.WriteLine("Enter PhoneNumber: ");
+            Console.WriteLine(UserValidation.checkEmail(email));
+            //User PhoneNumber
+            Console.WriteLine("Enter the phonenumber: ");
             string phonenumber = Console.ReadLine();
-            userValidation.ValidationPhoneNumber(phonenumber);
-
-            Console.WriteLine("Enter password 8 characters");
+            Console.WriteLine(UserValidation.checkMobileNumber(phonenumber));
+            //User Password
+            Console.WriteLine("Enter the password: ");
             string password = Console.ReadLine();
-            userValidation.ValidationPassword(password);
-
-            Console.WriteLine("Enter password 1 uppercase");
-            string Uppercase = Console.ReadLine();
-            userValidation.ValidationUppercase(Uppercase);
-
-            Console.WriteLine("Enter 1 numeric password");
-            string numericpassword = Console.ReadLine();
-            userValidation.ValidationNumericPassword(numericpassword);
+            Console.WriteLine(UserValidation.checkPassword(password));
         }
     }
 }
--- UserRegistrationProblem/UserValidation.cs
+++ UserRegistrationProblem/UserValidation.cs
@@ -9,24 +9,49 @@ namespace UserRegistrationProblem
 {
     public class UserValidation
     {
-        public const string FIRST_NAME_REGEX = "^[A-Z][a-zA-Z]{2}$";
+        public static string firstname;
+        //UC13
+        //Use Lambda Expression to validate User FirstName
+        public const string FIRST_NAME_REGEX = "^[A-Z]{1}[a-zA-Z]{2}$";
         public void ValidationFirstName(string firstname)
         {
             Regex regex = new Regex(FIRST_NAME_REGEX);
             bool result = regex.IsMatch(firstname);
             Console.WriteLine(result);
         }
-        //UC2
-        //LastName the First Letter Should Capital and minimum 3 Character
-        public const string LASTNAME_REGEX = "^[A-Z][a-zA-Z]{2}$";
+        public static Func<string, string> CheckFirstName = (firstname) =>
+        {
+            Regex regex = new Regex(FIRST_NAME_REGEX);
+            if (regex.IsMatch(firstname))
+            {
+                return "Firstname is valid";
+            }
+            else
+            {
+                return "Firstname is invalid";
+            }
+        };
+        //Use Lambda Expression to validate User LastName
+        public const string LASTNAME_REGEX = "^[A-Z]{1}[a-zA-Z]{2}$";
         public void ValidationLastName(string lastname)
         {
             Regex regex = new Regex(LASTNAME_REGEX);
             bool result = regex.IsMatch(lastname);
             Console.WriteLine(result);
         }
-        //UC3
-        //E-Mail validation
+        public static Func<string, string> CheckLastName = (lastname) =>
+        {
+            Regex regex = new Regex(LASTNAME_REGEX);
+            if (regex.IsMatch(lastname))
+            {
+                return "lastname is valid";
+            }
+            else
+            {
+                return "lastname is invalid";
+            }
+        };
+        //Use Lambda Expression to validate User Email
         public const string EMAIL_REGEX = "^[a-z0-9]+[@][a-zA-Z]+[.][a-zA-Z]{2,3}$";
         public void ValidationEmail(string email)
         {
@@ -34,16 +59,39 @@ namespace UserRegistrationProblem
             bool result = regex.IsMatch(email);
             Console.WriteLine(result);
         }
-        //UC4
-        // Pre define Mobile Format Validation
-        public const string MOBILENUMBER_REGEX = "^[0-9]+[\\s]+[0-9]{10}$";
+        public static Func<string, string> checkEmail = (email) =>
+        {
+            Regex regex = new Regex(EMAIL_REGEX);
+            if (regex.IsMatch(email))
+            {
+                return "email is valid";
+            }
+            else
+            {
+                return "email is invalid";
+            }
+        };
+        //Use Lambda Expression to validate User MobileNumber
+        public const string MOBILENUMBER_REGEX = "^[0-9]+[-]+[0-9]{10}$";
         public void ValidationPhoneNumber(string phonenumber)
         {
             Regex regex = new Regex(MOBILENUMBER_REGEX);
             bool result = regex.IsMatch(phonenumber);
             Console.WriteLine(result);
         }
-        //UC5 Password Validation minum 8 Character
+        public static Func<string, string> checkMobileNumber = (phonenumber) =>
+        {
+            Regex regex = new Regex(MOBILENUMBER_REGEX);
+            if (regex.IsMatch(phonenumber))
+            {
+                return "phonenumber is valid";
+            }
+            else
+            {
+                return "phonenumber is invalid";
+            }
+        };
+        //Use Lambda Expression to validate User Password
         public const string PASSWORD_REGEX = "[a-z,A-Z,0-9]{8,}$";
         public void ValidationPassword(string password)
         {
@@ -51,24 +99,18 @@ namespace UserRegistrationProblem
             bool result = regex.IsMatch(password);
             Console.WriteLine(result);
         }
-        //UC6
-        //Should  have at least 1 Upper Case
-        public const string UPPERCASE_REGEX = "^[A-Z][a-z]{7}$";
-        public void ValidationUppercase(string Uppercase)
+        public static Func<string, string> checkPassword = (password) =>
         {
-            Regex regex = new Regex(UPPERCASE_REGEX);
-            bool result = regex.IsMatch(Uppercase);
-            Console.WriteLine(result);
-        }
-        //UC7
-        //Should  have at least 1 numeric number in password
-        public const string NUMERICPASSWORD_REGEX = "^[a-z0-9A-Z]{8}$";
-        public void ValidationNumericPassword(string numericpassword)
-        {
-            Regex regex = new Regex(NUMERICPASSWORD_REGEX);
-            bool result = regex.IsMatch(numericpassword);
-            Console.WriteLine(result);
-        }
+            Regex regex = new Regex(PASSWORD_REGEX);
+            if (regex.IsMatch(password))
+            {
+                return "password is valid";
+            }
+            else
+            {
+                return "password is invalid";
+            }
+        };
     }
 }
 
